1c5a46a7e5fbcbede7f94141f6816ff39fe44cd6
WillianMSantos <william.marreiros2gmail.com>
2022-03-22 18:22:48 -0300

    Initial commit

---
+++ src/main/java/br/com/doublezerotri/gerenprojetos/GerenprojetosApplication.java
@@ -0,0 +1,13 @@
+package br.com.doublezerotri.gerenprojetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenprojetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenprojetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/doublezerotri/gerenprojetos/GerenprojetosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.doublezerotri.gerenprojetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenprojetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
